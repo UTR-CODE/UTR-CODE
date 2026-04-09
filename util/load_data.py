3ec98f848a5db4a635cfc23a308122573279e16e
Hexin <1968779700@qq.com>
2026-04-09 10:32:03 +0800

    fix bugs and typo

--- util/load_data.py
+++ util/load_data.py
@@ -6,21 +6,21 @@ from tqdm import tqdm
 import random
 import os
 
+
 class UTRDataset_combinedenv(Dataset):
-    def __init__(self, species = "human", sample="train", gene="train", dataset_dir="../training_rawdata"):
+    def __init__(self, species="human", sample="train", gene="train", dataset_dir="../training_rawdata"):
         train_gene = [i.strip() for i in open(os.path.join(dataset_dir, f"{species}_train_gene.txt"))]
         valid_gene = [i.strip() for i in open(os.path.join(dataset_dir, f"{species}_test_gene.txt"))]
-        data = pd.read_parquet(os.path.join(dataset_dir, f"{species}.parquet"))
+        data = pd.read_parquet(os.path.join(dataset_dir, f"{species}_TPM.parquet"))
 
-       
-        env_dir_data =  os.path.join(dataset_dir, f"../VAE_project/1024_6species/finetune/{species}/{species}_compressed.csv")
+        env_dir_data = os.path.join(dataset_dir,
+                                    f"../VAE_project/1024_6species/finetune/{species}/{species}_compressed.csv")
         self.mRNA_vae = pd.read_csv(env_dir_data, index_col=0)
         self.ohe_dict = {i.strip(): index for index, i in
                          enumerate(open(os.path.join(dataset_dir, f"{species}_transcript_ids.txt")))}
 
         train_sample = [i.strip() for i in open(os.path.join(dataset_dir, f"{species}_train_sample.txt"))]
         valid_sample = [i.strip() for i in open(os.path.join(dataset_dir, f"{species}_test_sample.txt"))]
-
 
         if sample == "train":
             data = data[data["sample_RPF_id"].isin(train_sample)]
@@ -31,19 +31,18 @@ class UTRDataset_combinedenv(Dataset):
         else:
             data = data[data["gene_id"].isin(valid_gene)]
 
-
         self.data = data.query("RNA_TPM > 0.1 ")
         self.data = self.data.reset_index(drop=True)
         folds = 5
-        #if species == "yeast":
-         #   folds = 3.5
-        #elif species == "chicken":
+        # if species == "yeast":
+        #   folds = 3.5
+        # elif species == "chicken":
         #    folds = 3
-        #elif species == "C.elegans":
-         #   folds = 3.5
-        
+        # elif species == "C.elegans":
+        #   folds = 3.5
+
         self.data["RNA_TPM"] = np.log1p(self.data["RNA_TPM"] * folds)
-        self.data["RPF_TPM"] = np.log1p(self.data["RPF_RPM"] * folds)
+        self.data["RPF_TPM"] = np.log1p(self.data["RPF_TPM"] * folds)
 
         self.seq_ohe = np.load(os.path.join(dataset_dir, f"{species}_utr_ohe.npz"))["ohe"]
 
@@ -63,20 +62,22 @@ class UTRDataset_combinedenv(Dataset):
                      "mRNA_vae": mRNA_vae}
         return to_return
 
+
 class LabeledDataset(Dataset):
     def __init__(self, dataset, species_id):
         self.dataset = dataset
         self.species_id = species_id  # 0: human, 1: mouse, 2: yeast
-    
+
     def __len__(self):
         return len(self.dataset)
-    
+
     def __getitem__(self, idx):
         data = self.dataset[idx]  # 原始数据和标签
         return data, self.species_id  # 额外返回物种 ID
 
+
 if __name__ == '__main__':
-    dataset = UTRDataset_combinedenv(species = "A.thaliana")
+    dataset = UTRDataset_combinedenv(species="A.thaliana")
 
     print("load finished")
     data_loader = DataLoader(dataset, batch_size=256, shuffle=True)
